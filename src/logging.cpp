63fdde7329ce859062c744d4fba911df449eb6cb
Bas Couwenberg <sebastic@debian.org>
2021-07-30 05:31:09 +0100

    Import osm2pgsql_1.5.1+ds.orig.tar.xz
    [dgit import orig osm2pgsql_1.5.1+ds.orig.tar.xz]

---
+++ src/logging.cpp
@@ -0,0 +1,19 @@
+/**
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * This file is part of osm2pgsql (https://osm2pgsql.org/).
+ *
+ * Copyright (C) 2006-2021 by the osm2pgsql developer community.
+ * For a full list of authors see the git log.
+ */
+
+#include "logging.hpp"
+
+thread_local unsigned int this_thread_num = 0;
+
+/// Global logger singleton
+logger the_logger{};
+
+/// Access the global logger singleton
+logger &get_logger() noexcept { return the_logger; }
+
